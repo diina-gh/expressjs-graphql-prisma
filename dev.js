ffb4865cc865f5ab0a83044de5ca77ab04eb4047
Seydina <dina3903@gmail.com>
2022-02-25 23:45:58 +0100

    update

--- dev.js
+++ dev.js
@@ -1,7 +1,3 @@
-// import app from "./api/index.js";
-// import consola from "consola";
-// import dotenv from "dotenv";
-
 const {httpServer} = require('./api/index')
 const consola = require('consola')
 const dotenv = require('dotenv')
