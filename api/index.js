ef7ec62aaaad4fd4d28e1dc873dec66774146a1c
Seydina <dina3903@gmail.com>
2022-02-25 18:05:40 +0100

    update

--- api/index.js
+++ api/index.js
@@ -3,6 +3,42 @@ import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
 import http from "http";
 import express from "express";
 import cors from "cors";
+import fs from "fs";
+import path from "path"
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+  }]
+  
+  // 1
+  const resolvers = {
+    Query: {
+      info: () => `This is the API of a Hackernews Clone`,
+      feed: () => links,
+    },
+    Mutation: {
+      // 2
+      post: (parent, args) => {
+    
+      let idCount = links.length
+  
+         const link = {
+          id: `link-${idCount++}`,
+          description: args.description,
+          url: args.url,
+        }
+        links.push(link)
+        return link
+      }
+    },
+  }
 
 
 const app = express();
@@ -10,21 +46,10 @@ app.use(cors());
 app.use(express.json());
 const httpServer = http.createServer(app);
 
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
-
-const resolvers = {
-  Query: {
-    hello: () => "world",
-  },
-};
 
 const startApolloServer = async(app, httpServer) => {
     const server = new ApolloServer({
-      typeDefs,
+      typeDefs: fs.readFileSync( path.join(__dirname, 'schema.graphql'),'utf8'),
       resolvers,
       plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
     });
--- dev.js
+++ dev.js
@@ -2,5 +2,9 @@ import app from "./api/index.js";
 import consola from "consola";
 import dotenv from "dotenv";
 
+// const app = require('./api/index')
+// const consola = require('consola')
+// const dotenv = require('dotenv')
+
 dotenv.config();
 app.listen(3000, () => consola.info("Server started"));
