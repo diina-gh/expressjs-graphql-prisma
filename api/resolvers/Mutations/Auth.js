67fb67649b4416284a6ccbae3ca982a00df398ab
Seydina <dina3903@gmail.com>
2022-02-27 03:48:01 +0100

    update

--- api/resolvers/Mutations/Auth.js
+++ api/resolvers/Mutations/Auth.js
@@ -20,13 +20,12 @@ export async function signup(parent, args, context, info) {
   }
   
   export async function login(parent, args, context, info) {
-    // 1
+    
     const user = await context.prisma.user.findUnique({ where: { email: args.email } })
     if (!user) {
       throw new Error('No such user found')
     }
   
-    // 2
     const valid = await bcrypt.compare(args.password, user.password)
     if (!valid) {
       throw new Error('Invalid password')
@@ -34,7 +33,6 @@ export async function signup(parent, args, context, info) {
   
     const token = jwt.sign({ userId: user.id }, APP_SECRET)
   
-    // 3
     return {
       token,
       user,
