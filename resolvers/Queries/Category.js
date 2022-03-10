878f91f9996738d0627ce8c7307a0cf69d9a8fea
Seydina <dina3903@gmail.com>
2022-03-11 00:45:30 +0100

    update

--- resolvers/Queries/Category.js
+++ resolvers/Queries/Category.js
@@ -9,6 +9,7 @@ export async function categories(parent, args, context, info) {
         { name: { contains: args.filter } },
         { desc: { contains: args.filter } },
         { long_desc: { contains: args.filter } },
+        {parentId: null}
       ],
     }
     : {}
@@ -27,11 +28,42 @@ export async function categories(parent, args, context, info) {
     const count = await context.prisma.category.count()
     return {count, categories}
   
+}
+
+export async function subcategories(parent, args, context, info) {
+
+  const skip = args.page && args.take ? (args.page - 1) * args.take : 0
+
+  const where = args.filter
+  ? {
+    OR: [
+      { name: { contains: args.filter } },
+      { desc: { contains: args.filter } },
+      { long_desc: { contains: args.filter } },
+      {parentId:  {not: null,}}
+    ],
   }
+  : {}
+
+  const categories = await context.prisma.category.findMany({
+    where,
+    include: {
+      parent: true,
+      childs: true
+    },
+    skip: skip,
+    take: args.take,
+    orderBy: args.orderBy,
+  })
+
+  const count = await context.prisma.category.count()
+  return {count, categories}
+
+}
   
-  export async function category(parent, args, context, info) {
-    return await context.prisma.category.findUnique({
-        where: {id: args.id,},
-        include: {parent: true,childs: true},
-    })
-  }
+export async function category(parent, args, context, info) {
+  return await context.prisma.category.findUnique({
+      where: {id: args.id,},
+      include: {parent: true,childs: true},
+  })
+}
