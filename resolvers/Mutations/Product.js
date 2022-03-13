3d824870c94e20551b880c749f880c7ffe514041
Seydina <dina3903@gmail.com>
2022-03-13 05:16:13 +0100

    update

--- resolvers/Mutations/Product.js
+++ resolvers/Mutations/Product.js
@@ -3,50 +3,40 @@ import { UserInputError} from "apollo-server-express";
 
 export async function saveProduct(parent, args, context, info) {
     
-  if(args.name == null){
-    throw new UserInputError("Veuillez donner une désignation.", {cstm_code: 'E3192013'});
-  }
-
-  else if(args.short_desc == null){
-    throw new UserInputError("Veuillez donner une description.", {cstm_code: 'E3192013'});
-  }
-
-  else if(args.unit == null){
-    throw new UserInputError("Veuillez donner l'unité du produit.", {cstm_code: 'E3192013'});
-  }
-
-  else if(args.unitweight == null){
-    throw new UserInputError("Veuillez donner le poids par unité.", {cstm_code: 'E3192013'});
-  }
-
-  else if(args.unitprice == null){
-    throw new UserInputError("Veuillez donner le prix par unité.", {cstm_code: 'E3192013'});
-  }
-
-  else if(args.categoryId == null){
-    throw new UserInputError("Veuillez choisir une catégorie.", {cstm_code: 'E3192013'});
-  }
-
-  else if(args.images == null || args.images?.length <= 0){
-    throw new UserInputError("Veuillez ajoutez des images.", {cstm_code: 'E3192013'});
-  }
+  if(args.name == null) throw new UserInputError("Veuillez donner une désignation.", {cstm_code: 'E3192013'});
   
-  else {
+  if(args.short_desc == null) throw new UserInputError("Veuillez donner une description.", {cstm_code: 'E3192013'});
+  
+  if(args.unit == null) throw new UserInputError("Veuillez donner l'unité du produit.", {cstm_code: 'E3192013'});
+  
+  if(args.unitweight == null) throw new UserInputError("Veuillez donner le poids par unité.", {cstm_code: 'E3192013'});
+  
+  if(args.unitprice == null) throw new UserInputError("Veuillez donner le prix par unité.", {cstm_code: 'E3192013'});
+  
+  if(args.categoryId == null) throw new UserInputError("Veuillez choisir une catégorie.", {cstm_code: 'E3192013'});
+  
+  if(args.brandId == null) throw new UserInputError("Veuillez choisir une marque.", {cstm_code: 'E3192013'});
 
-    for (let i = 0; i < args.images.length; i++) {
-        if(args.images[i].url == null || args.images[i].url == '') throw new UserInputError("Veuillez donner l'url de l'image " + (i+1), {cstm_code: 'E3192013'});
-        if(args.images[i].imageref == null || args.images[i].imageref == '') throw new UserInputError("Veuillez donner la référence de l'image " + (i+1), {cstm_code: 'E3192013'});
-    }
+  if(args.images == null || args.images?.length <= 0) throw new UserInputError("Veuillez ajoutez des images.", {cstm_code: 'E3192013'});
+  
+  let category = await context.prisma.category.findUnique({ where: { id: args.id } })
+  if(!category) throw new UserInputError("Cette catégorie n'éxiste pas.", {cstm_code: 'E3192013'});
 
-    if(args.variants != null && args.variants.length >= 0){
-        if(args.options == null || args.options.length <= 0) throw new UserInputError("Veuillez donner des options pour les variants de ce produit.", {cstm_code: 'E3192013'});
-    }
+  let brand = await context.prisma.brand.findUnique({ where: { id: args.id } })
+  if(!brand) throw new UserInputError("Cette marque n'éxiste pas.", {cstm_code: 'E3192013'});
 
-    if(args.id != null){
-      let product = await context.prisma.product.findUnique({ where: { id: args.id } })
-      if (!product) throw new UserInputError("Ce produit n'éxiste pas.", {cstm_code: 'E3192013'});
-    }
+  for (let i = 0; i < args.images.length; i++) {
+      if(args.images[i].url == null || args.images[i].url == '') throw new UserInputError("Veuillez donner l'url de l'image " + (i+1), {cstm_code: 'E3192013'});
+      if(args.images[i].imageref == null || args.images[i].imageref == '') throw new UserInputError("Veuillez donner la référence de l'image " + (i+1), {cstm_code: 'E3192013'});
+  }
 
+  if(args.variants != null && args.variants.length >= 0){
+      if(args.options == null || args.options.length <= 0) throw new UserInputError("Veuillez donner des options pour les variants de ce produit.", {cstm_code: 'E3192013'});
+  }
+
+  if(args.id != null){
+    let product = await context.prisma.product.findUnique({ where: { id: args.id } })
+    if (!product) throw new UserInputError("Ce produit n'éxiste pas.", {cstm_code: 'E3192013'});
   }
 
   const date = new Date()
@@ -54,7 +44,7 @@ export async function saveProduct(parent, args, context, info) {
   let links = []
   let links2 = []
   
-  const data= {name: args.name, short_desc: args.short_desc, long_desc: args.long_desc, status: args.status , activated: args.activated, unit: args.unit, unitweight: args.unitweight, unitprice: args.unitprice, taxable: args.taxable, order: args.order, images: {create: args.images}, category:{connect:{id: args.categoryId}} }
+  const data= {name: args.name, short_desc: args.short_desc, long_desc: args.long_desc, status: args.status , activated: args.activated, unit: args.unit, unitweight: args.unitweight, unitprice: args.unitprice, taxable: args.taxable, order: args.order, images: {create: args.images}, category:{connect:{id: args.categoryId}}, brandId: args.brandId }
 
   if(args.variants != null && args.variants.length >= 0){
 
