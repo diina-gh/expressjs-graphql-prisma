fa046b58cb1afc8cf72e203fc0b45d0a7ed2548f
Seydina <dina3903@gmail.com>
2022-03-19 02:53:12 +0100

    update

--- resolvers/Queries/Region.js
+++ resolvers/Queries/Region.js
@@ -35,7 +35,7 @@ export async function regions(parent, args, context, info) {
 
     if(args.id == null) return { __typename: "InputError", message: `Veuilez donner un identifiant`,};
 
-    let entity = await prisma.region.findUnique({where: {id: args.id,},include: {country: true, districts: true,},})
+    let entity = await context.prisma.region.findUnique({where: {id: args.id,},include: {country: true, districts: true,},})
     if(!entity) return { __typename: "InputError", message: `Cette région n'éxiste pas.`,};
    
     return { __typename: "Region", ...entity,};
