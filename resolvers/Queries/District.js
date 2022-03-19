104eda2cb0bf38aaf7902dab933aeb515480a39e
Seydina <dina3903@gmail.com>
2022-03-19 04:03:40 +0100

    update

--- resolvers/Queries/District.js
+++ resolvers/Queries/District.js
@@ -36,7 +36,7 @@ export async function districts(parent, args, context, info) {
 
     if(args.id == null) return { __typename: "InputError", message: `Veuilez donner un identifiant`,};
 
-    let entity = await prisma.district.findUnique({where: {id: args.id,},include: {region: {include:{country:true}}},})
+    let entity = await context.prisma.district.findUnique({where: {id: args.id,},include: {region: {include:{country:true}}},})
     if(!entity) return { __typename: "InputError", message: `Cette zone n'éxiste pas.`,};
    
     return { __typename: "District", ...entity,};
