300668109d6bdb20edd4ff5deb5c319a89330ed2
Seydina <dina3903@gmail.com>
2022-03-20 07:23:42 +0100

    update

--- resolvers/Queries/ShippingMethod.js
+++ resolvers/Queries/ShippingMethod.js
@@ -28,7 +28,7 @@ export async function shippingMethods(parent, args, context, info) {
 
     if(args.id == null) return { __typename: "InputError", message: `Veuilez donner un identifiant`,};
 
-    let entity = await prisma.shippingMethod.findUnique({where: {id: args.id,},})
+    let entity = await context.prisma.shippingMethod.findUnique({where: {id: args.id,},})
     if(!entity) return { __typename: "InputError", message: `Ce mode de livraison n'éxiste pas.`,};
 
     return { __typename: "ShippingMethod", ...entity,};
