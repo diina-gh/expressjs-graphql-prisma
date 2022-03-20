6c3adfb53a8062b6a471fb314f53cc183c716bda
Seydina <dina3903@gmail.com>
2022-03-20 05:13:57 +0100

    update

--- resolvers/Queries/Category.js
+++ resolvers/Queries/Category.js
@@ -67,7 +67,7 @@ export async function category(parent, args, context, info) {
 
   if(args.id == null) return { __typename: "InputError", message: `Veuilez donner un identifiant`,};
 
-  let entity =  await context.prisma.category.findUnique({where: {id: args.id,}, include: {parent: true, childs: true},})
+  let entity =  await context.prisma.category.findUnique({where: {id: args.id,}, include: {parent: true, childs: true, image: true},})
   if(!entity) return { __typename: "InputError", message: `Cette catégorie n'éxiste pas.`,};
    
   return { __typename: "Category", ...entity,};
