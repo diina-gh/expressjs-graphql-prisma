4b5ed3d24393bb7d06cae7f804f514fa1b1372d5
Seydina <dina3903@gmail.com>
2022-03-24 07:04:14 +0100

    update

--- resolvers/Mutations/Product.js
+++ resolvers/Mutations/Product.js
@@ -1,20 +1,9 @@
 export async function saveProduct(parent, args, context, info) {
 
-  var savedVariantIds, savedOptionIds, savedRelativeIds
-
   if(args.id != null){
 
     let product = await context.prisma.product.findUnique({ where: { id: args.id }, select: {id: true,}, })
     if (!product) return { __typename: "InputError", message: `Ce produit n'éxiste pas`,}; 
-
-    const savedVariants = await context.prisma.VariantsOnProducts.findMany({where: { productId: args.id }, select: {id: true,}, })
-    savedVariantIds = savedVariants.map(item =>  item.id);
-
-    const savedOptions = await context.prisma.OptionsOnProducts.findMany({where: { productId: args.id }, select: {id: true,}, })
-    savedOptionIds = savedOptions.map(item =>  item.id);
-
-    const savedRelatives = await context.prisma.product.findMany({where: { relatedId: args.id }, select: {id: true,}, })
-    savedRelativeIds = savedRelatives.map(item =>  item.id);
 
   }
     
@@ -46,8 +35,10 @@ export async function saveProduct(parent, args, context, info) {
 
   const date = new Date()
 
+  var variantDiffs, optionDiffs, relativeDiffs
   let links = []
   let links2 = []
+  let links3 = []
   
   const data= {name: args.name, desc: args.desc, activated: args.activated, unit: args.unit, unitweight: args.unitweight, unitprice: args.unitprice, order: args.order, categoryId:args.categoryId, brandId: args.brandId, gender:args.gender }
 
@@ -63,12 +54,38 @@ export async function saveProduct(parent, args, context, info) {
 
     data.variants = {create: links}
     data.options = {create: links2}
+  }
 
+  if(args.relatives != null && args.relatives.length > 0){
+    links3 = args.relatives.map(item => {id: item});
+    data.relatives = {connect: links3}
+  }
+
+  if(args.id != null){
+    const savedVariants = await context.prisma.VariantsOnProducts.findMany({where: { productId: args.id }, select: {id: true,}, })
+    const savedVariantIds = savedVariants.map(item =>  item.id);
+    variantDiffs = savedVariantIds.filter(x => !args.variants.includes(x)).concat(args.variants.filter(x => !savedVariantIds.includes(x)));
+
+    const savedOptions = await context.prisma.OptionsOnProducts.findMany({where: { productId: args.id }, select: {id: true,}, })
+    const savedOptionIds = savedOptions.map(item =>  item.id);
+    optionDiffs = savedOptionIds.filter(x => !args.options.includes(x)).concat(args.options.filter(x => !savedOptionIds.includes(x)));
+
+    const savedRelatives = await context.prisma.product.findMany({where: { relatedId: args.id }, select: {id: true,}, })
+    const savedRelativeIds = savedRelatives.map(item =>  item.id);
+    relativeDiffs = savedRelativeIds.filter(x => !args.relatives.includes(x)).concat(args.relatives.filter(x => !savedRelativeIds.includes(x)));
+
+    variantDiffs = variantDiffs.map(item => {id: item});
+    optionDiffs = optionDiffs.map(item => {id: item});
+    relativeDiffs = relativeDiffs.map(item => {id: item});
+
+    if(variantDiffs.length > 0) data.variants.disconnect = variantDiffs
+    if(optionDiffs.length > 0) data.options.disconnect = optionDiffs
+    if(relativeDiffs.length > 0) data.relatives.disconnect = relativeDiffs
   }
 
   let product = args.id ? 
-            await context.prisma.product.update({data: {...data, updatedat: date}}) :
-            await context.prisma.product.create({data: data})
+            await context.prisma.product.update({data: {...data, updatedat: date}, select: {id:true}}) :
+            await context.prisma.product.create({data: data, select: {id:true}})
 
   return { __typename: "Product", ...product,};
 
@@ -78,11 +95,10 @@ export async function deleteProduct(parent, args, context, info){
 
   if(args.id == null) return { __typename: "InputError", message: `Veuilez donner un identifiant`,};
 
-  let entity = await context.prisma.product.findUnique({ where: { id: args.id } })
+  let entity = await context.prisma.product.findUnique({ where: { id: args.id }, select: {id:true} })
   if(!entity) return { __typename: "InputError", message: `Ce produit n'éxiste pas`,};
     
-  const deletedEntity = await context.prisma.product.delete({where: {id: args.id,},})
+  const deletedEntity = await context.prisma.product.delete({where: {id: args.id,},  select: {id:true}})
   return { __typename: "Product", ...deletedEntity,};
-
 }
 
