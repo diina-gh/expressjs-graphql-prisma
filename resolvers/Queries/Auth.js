203ee435b9f9600e5149e5b7eb4005a91979a700
Seydina <dina3903@gmail.com>
2022-03-26 08:00:44 +0100

    update

--- resolvers/Queries/Auth.js
+++ resolvers/Queries/Auth.js
@@ -9,7 +9,8 @@ export async function users(parent, args, context, info) {
         { desc: { contains: args.filter } },
       ],
     }
-    : {}
+    : 
+    {customer: false}
 
     const selectedFields = new PrismaSelect(info).valueOf('users');
     const skip = args.page && args.take ? (args.page - 1) * args.take : 0
@@ -22,9 +23,34 @@ export async function users(parent, args, context, info) {
     const count = await context.prisma.user.count()
     return {count, users}
   
+}
+
+export async function clients(parent, args, context, info) {
+
+  const where = args.filter
+  ? {
+    OR: [
+      { name: { contains: args.filter } },
+      { desc: { contains: args.filter } },
+    ],
   }
+  : 
+  {customer: true}
+
+  const selectedFields = new PrismaSelect(info).valueOf('users');
+  const skip = args.page && args.take ? (args.page - 1) * args.take : 0
+  var query = {where, select: selectedFields.select, skip: skip,}
+
+  if(args.take) query.take = args.take
+  if(args.orderBy) query.orderBy = args.orderBy
+
+  const users = await context.prisma.user.findMany(query)
+  const count = await context.prisma.user.count()
+  return {count, users}
+
+}
   
-  export async function user(parent, args, context, info) {
+export async function user(parent, args, context, info) {
     if(args.id == null) return { __typename: "InputError", message: `Veuilez donner un identifiant`,};
 
     const selectedFields = new PrismaSelect(info).valueWithFilter('User');
@@ -33,4 +59,4 @@ export async function users(parent, args, context, info) {
     if(!entity) return { __typename: "InputError", message: `Cet utilisateur n'éxiste pas.`,};
   
     return { __typename: "User", ...entity,};
-  }
+}
