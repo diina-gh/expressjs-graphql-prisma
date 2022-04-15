80a862a49e5e2c9319fc31a6b6d38b265cd86d44
Seydina <dina3903@gmail.com>
2022-04-15 08:25:06 +0200

    update

--- resolvers/Mutations/Newsletter.js
+++ resolvers/Mutations/Newsletter.js
@@ -1,42 +1,37 @@
-import { PrismaSelect } from '@paljs/plugins';
-import { UserInputError} from "apollo-server-express";
-
 export async function saveNewsletter(parent, args, context, info) {
-    
-  if(args.email == null){
-    throw new UserInputError("Veuillez renseigner le champ adresse email.", {cstm_code: 'E3192013'});
-  }
-  else {
-    if(args.id == null){
-      let email = await context.prisma.newsletter.findUnique({ where: { email: args.email } })
-      if (email) throw new UserInputError("Vous vous etes déjà inscrit(e) à notre newsletter.", {cstm_code: 'E3192013'});
-    }
-    else{
-      let email = await context.prisma.newsletter.findUnique({ where: { id: args.id } })
-      if (!email) throw new UserInputError("Cette adresse email n'éxiste pas.", {cstm_code: 'E3192013'});
-    }
-  }
-
-  let newsletter = await context.prisma.newsletter.upsert({
-    where: {id: args.id ? args.id : 0,},
-    update: {email: args.email},
-    create: {email: args.email},
-  })
   
-  return newsletter
+  var query0 = { id: args.id }, query1 = { email: args.email}, query2 = {not: args.id,}
+
+  if(args.id != null){
+    let newsletter = await context.prisma.newsletter.findUnique({ where: query0 })
+    if (!newsletter) return { __typename: "InputError", message: `Cette adresse email n'éxiste pas`,};
+    query1.id = query2;
+  } 
+
+  if(args.email == null || args.email == '') return { __typename: "InputError", message: `Veuillez donner une adresse email.`,};
+
+  let row = await context.prisma.newsletter.findFirst({ where: query1 })
+  if(row) return { __typename: "InputError", message: `Vous vous êtes déjà inscrit(e) à notre newsletter`,};
+
+  const date = new Date()
+  var data = {email: args.email,}
+
+  let newsletter = args.id ? 
+    await context.prisma.newsletter.update({where: {id:args.id}, data: {...data, updatedat: date}}) :
+    await context.prisma.newsletter.create({data: data})
+
+    return { __typename: "Newsletter", ...newsletter,};
+
 }
 
 export async function deleteNewsletter(parent, args, context, info){
 
-  let entity = await context.prisma.newsletter.findUnique({ where: { id: args.id } })
+  if(args.id == null) return { __typename: "InputError", message: `Veuilez donner un identifiant`,};
 
-  if(!entity){
-    throw new UserInputError("Cette adresse email n'éxiste pas.", {cstm_code: 'E3192013'});
-  }
-  else{
-    const deletedEntity = await context.prisma.newsletter.delete({where: {id: args.id,},})
-    return deletedEntity
-  }
+  let entity = await context.prisma.newsletter.findUnique({ where: { id: args.id } })
+  if(!entity) return { __typename: "InputError", message: `Cette adresse email n'éxiste pas`,};
+
+  const deletedEntity = await context.prisma.newsletter.delete({where: {id: args.id,},})
+  return { __typename: "Newsletter", ...deletedEntity,};
 
 }
-
